a829d4877d2e514dcb1e39327db8671a3471fb7f
DeviFox <oleh.svyatnenko@gmail.com>
2020-06-03 23:50:59 +1100

    in progress

--- app/src/main/java/com/example/localdb/MainActivity.java
+++ app/src/main/java/com/example/localdb/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.localdb;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btn_add, btn_open;
+    private EditText  edText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edText = (EditText) findViewById(R.id.edText);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        btn_open = (Button) findViewById(R.id.btn_open);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+    }
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+    }
+
+
+        private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
